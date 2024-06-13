753ed6e6b9ba5979671ce9b817f4677b7c0b323b
shobitha-2003 <121032823+shobitha-2003@users.noreply.github.com>
2024-06-13 22:20:35 +0530

    Add files via upload

---
+++ pattern11.java
@@ -0,0 +1,12 @@
+public class pattern11 {
+    public static void main(String[] args) {
+        int a=65;
+        int n=4;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print((char)(a+j)+" ");
+            }
+System.out.println();
+        }
+    }
+}
